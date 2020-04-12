0931a50f17bf44d6a9865fe6f1c04b2ef3ae55c4
Coral <cvila98@gmail.com>
2020-04-12 20:22:13 +0200

    creation view user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_155204) do
+ActiveRecord::Schema.define(version: 2020_04_12_173347) do
 
   create_table "contributions", force: :cascade do |t|
     t.string "tipo", default: "url"
