c928728c36b75bcb7254d8e7711bdc0616387d13
ceraalex99 <ceraalex99@gmail.com>
2020-05-22 13:49:39 +0200

    liked en replies

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -3,7 +3,7 @@ class Interaction < Contribution
   def respostes
     results = []
     replies.each do |reply|
-      results << reply.as_json(except: [:updated_at, :title, :url, :tipo],:methods => [:type, :author,:respostes])
+      results << reply.as_json(except: [:updated_at, :title, :url, :tipo],:methods => [:type, :author,:respostes, :liked])
     end
     results
   end
